5859264347640638c74f7e9c2cbc5f52737e9c01
Sarpong Abasimi <sabasimi@deloitte.co.uk>
2020-06-10 21:21:10 +0100

    [SA] images

--- imageToDb/app/main/routes.py
+++ imageToDb/app/main/routes.py
@@ -1,7 +1,16 @@
-from flask import Blueprint
+import os
+from flask import Blueprint, render_template
+from app import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET'])
 def index():
-  return('hello world')
+  images = load_image()
+  return render_template('index.html', images=images)
+
+def load_image():
+  home = os.environ.get('HOME')
+  image_dir = os.path.join(home, 'Desktop', 'img')
+
+  return (os.listdir(image_dir))
--- imageToDb/app/models.py
+++ imageToDb/app/models.py
@@ -2,7 +2,4 @@ from app import db
 
 class Image(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  image_file = db.Column(db.String(20), nullable=True)
-
-  def __repr__(self):
-    return f'Image Id is {id} -> {image_file}'
+  image = db.Column(db.LargeBinary)
