70c33c51051dc94d74e70779d0840ce1a55d02d9
Sarpong Abasimi <sabasimi@deloitte.co.uk>
2020-06-29 23:24:35 +0100

    [SA] Created show page

--- flaskStatic/app/main/routes.py
+++ flaskStatic/app/main/routes.py
@@ -5,6 +5,13 @@ from app.products.products import product
 
 main: Type[Blueprint] = Blueprint('main', __name__)
 
-@main.route('/', methods = ['GET', 'POST'])
+@main.route('/', methods = ['GET'])
 def index() -> Text:
   return render_template('index.html', products=product['all_products'])
+
+@main.route('/show/<int:product_id>', methods =['GET'])
+def show(product_id: int) -> Text:
+  find_prouct_by_id = lambda x: x['product_id'] == product_id
+  products = product['all_products']
+  item = list(filter(find_prouct_by_id, products))
+  return render_template('show.html', item=item)
