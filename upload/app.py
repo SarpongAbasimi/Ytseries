88944fadb8aff68a403e05f544d76f212edaeb1a
Sarpong Abasimi <sabasimi@deloitte.co.uk>
2020-06-30 22:48:20 +0100

    [SA] Complete

--- upload/app.py
+++ upload/app.py
@@ -1,12 +1,21 @@
 import os 
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, send_file
 from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from io import BytesIO
 
 app = Flask(__name__)
 app.secret_key = b'secret'
+db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 
 BASE_PATH = os.path.abspath(os.environ.get('HOME'))
 ALLOWED_EXTENSION = {'txt', 'pdf', 'jpg', 'jpeg', 'gif', 'png'}
+
+class Item(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, unique=True, nullable=False)
+  data = db.Column(db.LargeBinary)
 
 def allowed_files(filename: str) -> bool:
   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSION
@@ -25,15 +34,26 @@ def index():
       return redirect(request.url)
 
     if file and allowed_files(file.filename):
-      filename = secure_filename(file.filename)
-      file.save(os.path.join(BASE_PATH, 'DESKTOP/whereToUploadTo', filename))
+      # filename = secure_filename(file.filename)
+      # file.save(os.path.join(BASE_PATH, 'DESKTOP/whereToUploadTo', filename))
+
+      new_file = Item(name=file.filename, data=file.read())
+      db.session.add(new_file)
+      db.session.commit()
+
       flash('File uploaded successfully')
-      return redirect(url_for('download'))
+      return redirect(url_for('index'))
   return render_template('index.html')
 
-@app.route('/download', methods=['GET'])
-def download():
-  return 'Download Page'
+@app.route('/files', methods=['GET'])
+def files():
+  items = Item().query.all()
+  return render_template('files.html', items=items)
+
+@app.route('/download/<int:id>', methods=['GET'])
+def download(id):
+  item = Item().query.filter_by(id=id).first()
+  return send_file(BytesIO(item.data), mimetype='image/png', as_attachment=True, attachment_filename=item.name)
 
 if __name__ == '__main__':
   app.run(debug=True)
