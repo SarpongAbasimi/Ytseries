e52604b755350877ef4625a70e50fbe68d1500d5
Sarpong Abasimi <sabasimi@deloitte.co.uk>
2020-08-09 17:13:47 +0100

    [SA] scala series

---
+++ scala/HelloWorld/src/main/scala/HelloWorld.scala
@@ -0,0 +1,16 @@
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    var name = "chris"
+    val anotherName = "Ben"
+    println("Sam")
+    println("Chris")
+    print("Mark")
+    print("Ben\n")
+
+    println(s"Hello my name is $anotherName")
+  }
+
+  def name: String = {
+    "hello"
+  }
+}
